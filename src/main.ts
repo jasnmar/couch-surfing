7354ec519b95d577be10ce0ee8ede9ce45177350
jasnmar <jasnmar@gmail.com>
2024-06-16 13:10:25 -0400

    Challenge to display review count

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,33 @@
-import "./style.css" 
+import "./style.css"
 
-// Benefits of TypeScript
-// challenges:
-// - give the screen's innerHTML a string
-// - change the two input values to numbers
+// Number Types mini-challenge
+// Write a function that will only accept numbers and attend to 
+// all TypeScript weakness flags.
+const reviewTotalDisplay = document.querySelector('#reviews') as HTMLHeadingElement
 
-const button = document.querySelector('.button') as HTMLButtonElement
-const firstInput = document.querySelector('#first-input') as HTMLInputElement
-const secondInput = document.querySelector('#second-input') as HTMLInputElement
-export const screen = document.querySelector('.screen') as HTMLDivElement
- 
-function addNumbers(a: number,b: number) {
-    const result: number = a + b
-     screen.innerHTML = result.toString()   
+function displayReviewCount(count: number) {
+  reviewTotalDisplay.textContent = count.toString()
 }
- 
-button.addEventListener('click', () => addNumbers(parseInt(firstInput.value), parseInt(secondInput.value)))
+
+const reviews = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+]
+
+displayReviewCount(reviews.length)
