0be0f73c049434694e410d5ef8c13d628a457aba
jasnmar <jasnmar@gmail.com>
2024-06-16 13:02:01 -0400

    Types for everything in adder

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
-import './style.css'
+import "./style.css" 
 
-let age: number = 100
-let firstName: string = 'Angela'
-let busy: boolean = true
+// Benefits of TypeScript
+// challenges:
+// - give the screen's innerHTML a string
+// - change the two input values to numbers
 
-console.log(`${firstName} is ${busy}, and is ${age} years old`)
+const button = document.querySelector('.button') as HTMLButtonElement
+const firstInput = document.querySelector('#first-input') as HTMLInputElement
+const secondInput = document.querySelector('#second-input') as HTMLInputElement
+export const screen = document.querySelector('.screen') as HTMLDivElement
+ 
+function addNumbers(a: number,b: number) {
+    const result: number = a + b
+     screen.innerHTML = result.toString()   
+}
+ 
+button.addEventListener('click', () => addNumbers(parseInt(firstInput.value), parseInt(secondInput.value)))
