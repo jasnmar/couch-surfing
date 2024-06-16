24b3ac6b6c79ec1abdb8ca5dd05989bce4cd0fb8
jasnmar <jasnmar@gmail.com>
2024-06-16 13:38:05 -0400

    Fixing the site

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import "./style.css"
 
-// Number Types mini-challenge
-// Write a function that will only accept numbers and attend to 
-// all TypeScript weakness flags.
+// Fixing the Website
+
+// Can you fix my code to show 'Welcome back Bobby' in the Nav Bar for 
+// our couch surfing website? Do this by assigning types to the parameters
+// we pass to our populateUser function, so that we can be aware of
+// errors in our user objects in the future.
+const returningUserDisplay = document.querySelector('#returning-user') as HTMLSpanElement
+const userNameDisplay = document.querySelector('#user') as HTMLSpanElement
+
 const reviewTotalDisplay = document.querySelector('#reviews') as HTMLHeadingElement
 
 function displayReviewCount(count: number, reviewer: string, loyalty: boolean) {
@@ -31,3 +37,23 @@ const reviews = [
 ]
 
 displayReviewCount(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+
+// const you = {
+//     userName: {firstName: 'Bobby', lastName: 'Brown'},
+//     isReturning: 'yes',
+// }
+
+const you = {
+    userName: {firstName: 'Bobby', lastName: 'Brown'},
+    isReturning: true,
+}
+
+function populateUser(isReturning: boolean, userName: string ) {
+  if (isReturning){
+      returningUserDisplay.innerHTML = 'back'
+  }
+  userNameDisplay.innerHTML = userName
+}
+
+populateUser(you.isReturning, you.userName.firstName)
